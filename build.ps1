9478d8362955a21c981bd7f8365e657b4120d555
Josh Rickard <josh.rickard@phishme.com>
2018-07-09 22:46:54 -0400

    updated CI scripts

--- build.ps1
+++ build.ps1
@@ -1,68 +1,92 @@
-[cmdletbinding()]
-Param (
-    [switch]$CleanBuild,
-    [switch]$InstallModule,
-    [switch]$PublishModule,
-    [string]$ApiKey,
-    [string[]]$PowerShellModules = @("Pester", "Psake", "BuildHelpers", "Plaster", "Platyps"),
-    [string[]]$PackageProviders = @('NuGet', 'PowerShellGet')
-)
+# Line break for readability in AppVeyor console
+Write-Host -Object ''
 
-
-if ($CleanBuild) {
-    If (Get-Module -Name CaptureIT) {
-        Remove-Module -Name CaptureIT -Force
-    }
+# Make sure we're using the Master branch and that it's not a pull request
+# Environmental Variables Guide: https://www.appveyor.com/docs/environment-variables/
+if ($env:APPVEYOR_REPO_BRANCH -ne 'master') {
+    Write-Warning -Message "Skipping version increment and publish for branch $env:APPVEYOR_REPO_BRANCH"
 }
-
-if ($InstallModule) {
-
-    # Install package providers for PowerShell Modules
-    ForEach ($Provider in $PackageProviders) {
-        If (!(Get-PackageProvider $Provider -ErrorAction SilentlyContinue -Force -ForceBootstrap)) {
-            Install-PackageProvider $Provider -Force -ForceBootstrap -Scope CurrentUser
-        }
-    }
-
-    # Install the PowerShell Modules
-    ForEach ($Module in $PowerShellModules) {
-        If (!(Get-Module -ListAvailable $Module -ErrorAction SilentlyContinue)) {
-            Install-Module $Module -Scope CurrentUser -Force -Repository PSGallery
-        }
-        Import-Module $Module
-    }
-
-    Push-Location $PSScriptRoot
-    Write-Output "Retrieving Build Variables"
-    Get-ChildItem -Path env:\bh* | Remove-Item
-    Set-BuildEnvironment
-
-    if (-not(Get-Module -Name CaptureIT)) {
-        Import-Module .\CaptureIT\CaptureIT.psm1
-    }
+elseif ($env:APPVEYOR_PULL_REQUEST_NUMBER -gt 0) {
+    Write-Warning -Message "Skipping version increment and publish for pull request #$env:APPVEYOR_PULL_REQUEST_NUMBER"
 }
+else {
+    # We're going to add 1 to the revision value since a new commit has been merged to Master
+    # This means that the major / minor / build values will be consistent across GitHub and the Gallery
+    Try {
+        # This is where the module manifest lives
+        $manifestPath = '.\CaptureIT\CaptureIT.psd1'
 
-if ($PublishModule) {
-    If (![string]::IsNullOrEmpty($ApiKey)) {
+        # Start by importing the manifest to determine the version, then add 1 to the revision
+        $manifest = Test-ModuleManifest -Path $manifestPath
+        [System.Version]$version = $manifest.Version
+        Write-Output "Old Version: $version"
+        [String]$newVersion = New-Object -TypeName System.Version -ArgumentList ($version.Major, $version.Minor, $version.Build, $env:APPVEYOR_BUILD_NUMBER)
+        Write-Output "New Version: $newVersion"
 
-        if (-not(Get-Module -Name CaptureIT)) {
-            Import-Module .\CaptureIT\CaptureIT.psm1
+        # Update the manifest with the new version value and fix the weird string replace bug
+        $functionList = ((Get-ChildItem -Path .\CaptureIT\Public).BaseName)
+        $splat = @{
+            'Path'              = $manifestPath
+            'ModuleVersion'     = $newVersion
+            'FunctionsToExport' = $functionList
+            'Copyright'         = "(c) 2015-$( (Get-Date).Year ) CaptureIT. All rights reserved."
         }
-        
-        $props = @{
-            Name = 'CaptureIT'
-            Repository = 'PSGallery'
-            NuGetApiKey = $ApiKey
-            Tags        = 'GIF', 'Screenshot', 'Record'
-            LicenseUri  = 'https://github.com/MSAdministrator/CaptureIT/blob/master/LICENSE.md'
-            ProjectUri  = 'https://github.com/MSAdministrator/CaptureIT'
+        Update-ModuleManifest @splat
+        (Get-Content -Path $manifestPath) -replace 'PSGet_CaptureIT', 'CaptureIT' | Set-Content -Path $manifestPath
+        (Get-Content -Path $manifestPath) -replace 'NewManifest', 'CaptureIT' | Set-Content -Path $manifestPath
+        (Get-Content -Path $manifestPath) -replace 'FunctionsToExport = ', 'FunctionsToExport = @(' | Set-Content -Path $manifestPath -Force
+        (Get-Content -Path $manifestPath) -replace "$($functionList[-1])'", "$($functionList[-1])')" | Set-Content -Path $manifestPath -Force
+    }
+    catch {
+        throw $_
+    }
+
+    # Create new markdown and XML help files
+    Write-Host "Building new function documentation" -ForegroundColor Yellow
+    Import-Module -Name "$PSScriptRoot\..\CaptureIT" -Force
+    New-MarkdownHelp -Module CaptureIT -OutputFolder '.\docs\' -Force
+    New-ExternalHelp -Path '.\docs\' -OutputPath '.\CaptureIT\en-US\' -Force
+    . .\docs.ps1
+    Write-Host -Object ''
+
+    # Publish the new version to the PowerShell Gallery
+    Try {
+        # Build a splat containing the required details and make sure to Stop for errors which will trigger the catch
+        $PM = @{
+            Path        = '.\CaptureIT'
+            NuGetApiKey = $env:NuGetApiKey
+            ErrorAction = 'Stop'
+            Tags         = 'GIF', 'Screenshot', 'Record'
+            LicenseUri   = 'https://github.com/MSAdministrator/CaptureIT/blob/master/LICENSE.md'
+            ProjectUri   = 'https://github.com/MSAdministrator/CaptureIT'
             ReleaseNotes = 'Initial release to the PowerShell Gallery'
         }
 
-        Publish-Module @props
+        Publish-Module @PM
+        Write-Host "CaptureIT PowerShell Module version $newVersion published to the PowerShell Gallery." -ForegroundColor Cyan
     }
-    else {
-        Write-Warning -Message 'Unable to publish module because no API key was provided'
-        exit -1
+    Catch {
+        # Sad panda; it broke
+        Write-Warning "Publishing update $newVersion to the PowerShell Gallery failed."
+        throw $_
+    }
+
+    # Publish the new version back to Master on GitHub
+    Try {
+        # Set up a path to the git.exe cmd, import posh-git to give us control over git, and then push changes to GitHub
+        # Note that "update version" is included in the appveyor.yml file's "skip a build" regex to avoid a loop
+        $env:Path += ";$env:ProgramFiles\Git\cmd"
+        Import-Module posh-git -ErrorAction Stop
+        git checkout master
+        git add --all
+        git status
+        git commit -s -m "Update version to $newVersion"
+        git push origin master
+        Write-Host "CaptureIT PowerShell Module version $newVersion published to GitHub." -ForegroundColor Cyan
+    }
+    Catch {
+        # Sad panda; it broke
+        Write-Warning "Publishing update $newVersion to GitHub failed."
+        throw $_
     }
 }
